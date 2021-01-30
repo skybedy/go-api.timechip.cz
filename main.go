877cdd8202fc56805f5f5836ca5228962402060b
skybedy <skybedy@gmail.com>
2021-01-30 21:10:40 +0100

    ..

--- main.go
+++ main.go
@@ -61,16 +61,3 @@ func main() {
 		log.Fatal(err)
 	}
 }
-
-/*
-package conf
-
-const DbDriver = "mysql"
-const DbUser = "skybedy"
-const DbPass = "mk1313life"
-const DbName = "timechip_cz"
-
-const AppPath = "."
-
-//const AppPath = "/var/www/timechip.cz/go-api.timechip.cz"
-*/
